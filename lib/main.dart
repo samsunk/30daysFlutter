8c5d52e740c4834c9f1c7e6020d33f517d6dcb5d
ShyamKhatiwada <samsunk03@gmail.com>
2021-08-13 00:01:40 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Material(
+        child: Center(
+          child: Container(
+            child:Text(" welcome shyam"),
+          ),
+        ),
+      ),
+    );
+  }
+}
